3c192d0cf4c65d81af2988756273d4c35a7762c0
Ryan Woods <ryanwoods477@yahoo.co.uk>
2019-10-02 11:29:19 +0100

    Clean up
    Cleaned Up files and created server and client files. started work on testing world generation.

---
+++ server/WorldGen.js
@@ -0,0 +1,21 @@
+var width = 10;
+var height = 10;
+
+var map = [width][height];
+var trueChance = 0.45;
+
+for(var x = 0; x<width; x++){
+    for(var y = 0; x<height; y++){
+        if((Math.random) < trueChance){
+            map[x][y] = true;
+        }
+    }
+}
+
+function doStep(){
+
+}
+
+function trueNeighbours(map, x, y){
+
+}
