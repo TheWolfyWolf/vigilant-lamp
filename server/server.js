ab4be7ab4c8b6f7251adbb6e9476566052183ead
fs65 <55656397+fs65@users.noreply.github.com>
2019-10-05 20:55:54 +0100

    adding code to show new connection

--- server/server.js
+++ server/server.js
@@ -34,3 +34,7 @@ app.get('/messages', (req, res) => {
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+io.on('connection', () =>{
+    console.log('a user is connected')
+   })
