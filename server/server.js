3d9154978e8a5af8d18428b54482abee505b5502
fs65 <55656397+fs65@users.noreply.github.com>
2019-10-05 21:09:23 +0100

    comments added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+//creates the server connection
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
    });
 
    app.use(express.static(__dirname));
+
+   //makes the connection to the database of messages from users
    var mongoose = require('mongoose');
    var dbUrl = 'https://cloud.mongodb.com/v2/5d98d90ca6f23936842d9eb5#metrics/replicaSet/5d98d9a879358e851d5b0563/explorer/in-game-chat/messages/find'
    mongoose.connect(dbUrl , (err) => { 
@@ -13,28 +16,34 @@ var server = app.listen(3000, () => {
  })
  var Message = mongoose.model('Message',{ name : String, message : String})
 
+ //body parser so the messages can be extracted 
  var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
+//this function retrives the messages from the database
 app.get('/messages', (req, res) => {
     Message.find({},(err, messages)=> {
       res.send(messages);
     })
   })
-
-  app.post('/messages', (req, res) => {
+//once the messages are pulled from the database they are then pushed to the server and posted
+app.post('/messages', (req, res) => {
     var message = new Message(req.body);
     message.save((err) =>{
       if(err)
         sendStatus(500);
+      io.emit('message', req.body);
       res.sendStatus(200);
     })
   })
 
+
+  //the socket.io is used to allow real time commuication between the web-client and the server 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//shows new connection of a new user to server
 io.on('connection', () =>{
     console.log('a user is connected')
    })
