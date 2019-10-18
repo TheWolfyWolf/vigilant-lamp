4695159a696b4a73d952cbe3f79d55221f0ff6ae
Ben Jack <ben@benjack.co.uk>
2019-10-18 13:37:45 +0100

    added bump script

--- public/index.php
+++ public/index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.1.5/pixi.min.js"></script>
-    <script src="pixi.js"></script>
-    <script src="WorldGen.js"></script>
-    <script src="engine.js"></script>
-    <style>
-    html {
-      background-color: #555;
-    }
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.1.5/pixi.min.js"></script>
+        <script src="bump.js"></script>
 
-    #container {
-      width: 90vw;
-      height: 90vh;
-      top: 5vh;
-      left: 5vw;
-      position: relative;
-    }
-    </style>
-  </head>
-  <body>
+        <script src="pixi.js"></script>
+        <script src="WorldGen.js"></script>
+        <script src="engine.js"></script>
+        <style>
+            html {
+                background-color: #555;
+            }
+
+            #container {
+                width: 90vw;
+                height: 90vh;
+                top: 5vh;
+                left: 5vw;
+                position: relative;
+            }
+        </style>
+    </head>
+    <body>
     <div id="container"></div>
-  </body>
-</html>
+    </body>
+    </html>
