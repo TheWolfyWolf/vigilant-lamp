8b8c7ff239e95e57aa4ddeb0fafd28a4cfee79dd
fs65 <55656397+fs65@users.noreply.github.com>
2019-10-20 14:36:34 +0100

    update server.js to mysql format

--- server/server.js
+++ server/server.js
@@ -1,24 +1,65 @@
+//start of creating the databse connection
 var mysql = require('mysql');
 
+
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "fraser.squash@gmail.com",
-  password: "Fujitsu147"
+  connectionLimit: 50,
+  host: "sql2.freesqldatabase.com",
+  user: "sql2309161",
+  password: "rA7%lV2!",
+  database: "sql2309161"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
+//creates he tables in the mysql database
+  var sql = "CREATE TABLE messages (name VARCHAR(255), message VARCHAR(255))";
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("Table created");
   });
+});
 
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE Messages (name VARCHAR(255), message VARCHAR(255))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
+//end
+
+//start of CONNECTION SERVER
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, () => {
+  console.log("server is running on port", server.address().port);
+ });
+
+ app.use(express.static(__dirname));
+
+ var Message = ("Message",{ name: String, message: String });
+ 
+ var bodyParser = require("body-parser")
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: false}))
+
+ //fucntion gets the messages from the database
+ app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+//function then posts the messages to the server
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+io.on("connection", () =>{
+  console.log("a user is connected")
+ })
